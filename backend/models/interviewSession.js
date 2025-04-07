b805c0863d0f3a7d313dff8a7a67081da382b4d5
Mohammad Hasan <mohammadhasan707053@gmail.com>
2025-04-07 17:31:23 +0530

    schema updated

--- backend/models/interviewSession.js
+++ backend/models/interviewSession.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 const interviewSessionSchema = mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Interview" }],
-  answers: [{ type: String }],
+  qaPairs: [
+    {
+      question: { type: String, required: true },
+      answer: { type: String }, 
+    },
+  ],
   scores: [{ type: Number }], 
   duration: { type: Number }, 
   result: { type: String }, 
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,22 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String }, 
+  email: { type: String, required: true, unique: true,
+     match: [
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    "Please enter a valid email",
+],
+ },
+ password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password must be at least 6 characters"],
+    match: [
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+      "Password must include uppercase, lowercase, number and special character",
+    ],
+  },
+
   googleId: { type: String }, 
   avatar: { type: String }, 
   role: { type: String, enum: ["free", "paid"], default: "free" },
